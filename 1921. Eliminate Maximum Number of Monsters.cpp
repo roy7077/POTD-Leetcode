aa484b1789e2f432a802ad7f4f2b0fc352df72c9
Sagar ray <raysagar807@gmail.com>
2023-11-07 13:16:40 +0530

    7th nov 2023 medium

---
+++ 1921. Eliminate Maximum Number of Monsters.cpp
@@ -0,0 +1,21 @@
+// Time complexity - O(NlogN)
+// Space complexity- O(1)
+class Solution {
+    public:
+    int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
+        
+        int n=dist.size();
+        for(int i=0;i<n;i++)
+        dist[i]=((dist[i]-1)/speed[i]);
+            
+        
+        sort(dist.begin(),dist.end());
+        for(int i=0;i<n;i++)
+        {
+            if(i>dist[i])
+            return i;
+        }
+        
+        return n;
+    }
+};
