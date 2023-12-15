484ff2c9badd21ba5deac1adfe7261b642eb0e4f
Sagar ray <raysagar807@gmail.com>
2023-12-15 11:46:46 +0530

    15th of dec 2023 easy

---
+++ 1436. Destination City.cpp
@@ -0,0 +1,18 @@
+// Time complexity - O(N)
+// Space complexity- O(N)
+class Solution {
+    public:
+    string destCity(vector<vector<string>>& paths) {
+        unordered_map<string,int> mp;
+        for(auto it:paths)
+        mp[it[0]]++;
+        
+        for(auto it:paths)
+        {
+            if(!mp.count(it[1]))
+            return it[1];
+        }
+        
+        return "jai shree ram";
+    }
+};
