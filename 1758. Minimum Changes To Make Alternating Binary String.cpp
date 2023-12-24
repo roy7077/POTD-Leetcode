06e793aaeecc19fdc1408507711b6ecb32edef70
Sagar ray <raysagar807@gmail.com>
2023-12-24 15:47:14 +0530

    24th of dec 2023 easy

---
+++ 1758. Minimum Changes To Make Alternating Binary String.cpp
@@ -0,0 +1,12 @@
+// Time complexity - O(N)
+// Space complexity- O(1)
+class Solution {
+    public:
+    int minOperations(string s) {
+        int res = 0, n = s.length();
+        for (int i = 0; i < n; ++i)
+            if (s[i] - '0' != i % 2)
+                res++;
+        return min(res, n - res);
+    }
+};
