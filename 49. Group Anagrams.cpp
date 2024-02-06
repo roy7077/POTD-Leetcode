6fcc2f7a853b3968b9e9075bde95b5572ff07921
Sagar ray <raysagar807@gmail.com>
2024-02-06 19:29:28 +0530

    6th of feb 2024 medium

---
+++ 49. Group Anagrams.cpp
@@ -0,0 +1,31 @@
+/*
+   Time complexity - O(N*NlogN)
+   Space complexity- O(N+N)
+   Sorting & unordered map
+*/
+
+class Solution {
+    public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+        unordered_map<string,vector<int>> mp;
+        for(int i=0;i<strs.size();i++)
+        {
+            string temp=strs[i];
+            sort(temp.begin(),temp.end());
+            
+            mp[temp].push_back(i);
+        }
+        
+        vector<vector<string>> ans;
+        for(auto it:mp)
+        {
+            vector<string> tempAns;
+            for(int i=0;i<it.second.size();i++)
+            tempAns.push_back(strs[it.second[i]]);
+            
+            ans.push_back(tempAns);
+        }
+        return ans;
+    }
+};
