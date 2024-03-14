90f55fe10fa19cb1dc4f1ce6e325202d814b57c8
Sagar ray <raysagar807@gmail.com>
2024-03-14 10:12:11 +0530

    14th of march 2024 medium

---
+++ 930. Binary Subarrays With Sum.cpp
@@ -0,0 +1,21 @@
+// Unordered map
+// Time complexity - O(N)
+// Space complexity- O(N)
+class Solution {
+    public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        unordered_map<int,int> mp;
+        int sum=0;
+        int cnt=0;
+        mp[0]=1;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+            if(mp.count(sum-goal))
+            cnt+=mp[sum-goal];
+            
+            mp[sum]++;
+        }
+        return cnt;
+    }
+};
