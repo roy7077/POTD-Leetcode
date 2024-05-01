87a55821119166cb8cf375ddc313bd510082d062
Sagar ray <raysagar807@gmail.com>
2024-05-01 10:03:54 +0530

    1st of may 2024 easy

---
+++ 2000. Reverse Prefix of Word.cpp
@@ -0,0 +1,16 @@
+// Time complexity - O(N)
+// Space complexity- O(1)
+class Solution {
+    public:
+    string reversePrefix(string word, char ch) {
+        for(int i=0;i<word.length();i++)
+        {
+            if(word[i]==ch)
+            {
+                reverse(word.begin(),word.begin()+(i+1));
+                break;
+            }
+        }
+        return word;
+    }
+};
